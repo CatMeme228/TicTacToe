be3f3574473eddca86846f01e0823997e3909078
Cat_meme <tolabelaev1251@gmail.com>
2023-01-08 20:52:46 +0300

    var renaming

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ class Field():
         print(self.cells[3], self.cells[4], self.cells[5])
         print(self.cells[6], self.cells[7], self.cells[8])
 
-    def setSymdol(self, symbol, i, j):
-        self.cells[j*3+i] = symbol
+    def setSymdol(self, symbol, y, x):
+        self.cells[x*3+y] = symbol
 
 
 myField= Field()
@@ -17,35 +17,35 @@ myField.print()
 
 for i in range(10):
     if myField.cells[0] == myField.cells[1] == myField.cells[2] == '0' or myField.cells[0] == myField.cells[1] == myField.cells[2] == 'x':
-        print(myField.cells[0], 'Wins')
+        print(myField.cells[0], 'Won')
         exit()
     elif myField.cells[3] == myField.cells[4] == myField.cells[5] == '0' or myField.cells[3] == myField.cells[4] == myField.cells[5] == 'x':
-        print(myField.cells[3], 'Wins')
+        print(myField.cells[3], 'Won')
         exit()
     elif myField.cells[6] == myField.cells[7] == myField.cells[8] == '0' or myField.cells[6] == myField.cells[7] == myField.cells[8] == 'x':
-        print(myField.cells[6], 'Wins')
+        print(myField.cells[6], 'Won')
         exit()
     elif myField.cells[0] == myField.cells[3] == myField.cells[6] == '0' or myField.cells[0] == myField.cells[3] == myField.cells[6] == 'x':
-        print(myField.cells[0], 'Wins')
+        print(myField.cells[0], 'Won')
         exit()
     elif myField.cells[1] == myField.cells[4] == myField.cells[7] == '0' or myField.cells[1] == myField.cells[4] == myField.cells[7] == 'x':
-        print(myField.cells[1], 'Wins')
+        print(myField.cells[1], 'Won')
         exit()
     elif myField.cells[2] == myField.cells[5] == myField.cells[8] == '0' or myField.cells[2] == myField.cells[5] == myField.cells[8] == 'x':
-        print(myField.cells[2], 'Wins')
+        print(myField.cells[2], 'Won')
         exit()
     elif myField.cells[0] == myField.cells[4] == myField.cells[8] == '0' or myField.cells[0] == myField.cells[4] == myField.cells[8] == 'x':
-        print(myField.cells[0], 'Wins')
+        print(myField.cells[0], 'Won')
         exit()
     elif myField.cells[2] == myField.cells[4] == myField.cells[6] == '0' or myField.cells[2] == myField.cells[4] == myField.cells[6] == 'x':
-        print(myField.cells[2], 'Wins')
+        print(myField.cells[2], 'Won')
         exit()
     elif i == 9:
-        print('No winer')
+        print('Draw')
 
     else:
-        inpSymbol = input()
-        inpj = int(input())
-        inpi = int(input())
-        myField.setSymdol(inpSymbol, inpi-1, inpj-1)
+        inp_Symbol = input()
+        inp_x = int(input())
+        inp_y = int(input())
+        myField.setSymdol(inp_Symbol, inp_y-1, inp_x-1)
         myField.print()
